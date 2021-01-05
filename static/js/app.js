6c4084ea168db9de40b1644070127b4d18a6c564
pparashar68 <pparashar68@gmail.com>
2021-01-04 22:11:15 -0800

    added the code to plot bar chart using plotly

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
 // Functions for events
-function getDemoInfo(id) {
+function getDemoInfo(id_number) {
   // Search jason file for input names value
-  console.log(id);
+  // console.log(id);
   d3.json("samples.json").then((data) => {
     metadata = data.metadata;
-    result = metadata.filter((meta) => meta.id.toString() === id)[0];
+    result = metadata.filter((meta) => meta.id.toString() === id_number)[0];
     console.log(result);
 
     let demographicInfo = d3.select("#sample-metadata");
@@ -17,6 +17,50 @@ function getDemoInfo(id) {
 }
 function optionChanged(id) {
   getDemoInfo(id);
+  plotCharts(id);
+}
+function plotCharts(id_number) {
+  d3.json("samples.json").then((data) => {
+    //console.log(data);
+    sampledata = data.samples;
+    result = sampledata.filter((meta) => meta.id.toString() === id_number)[0];
+    //console.log(result);
+
+    let sampleValues = result.sample_values.slice(0, 10).reverse();
+    //console.log(sampleValues);
+    let labels = result.otu_labels.slice(0, 10);
+    // console.log(labels);
+    let top10_otu = result.otu_ids.slice(0, 10).reverse();
+    // console.log(top10_otu);
+    let otu_id = top10_otu.map((d) => "OTU " + d);
+    // console.log(data.samples[0].otu_ids);
+    let trace = {
+      x: sampleValues,
+      y: otu_id,
+      text: labels,
+      marker: {
+        color: "blue",
+      },
+      type: "bar",
+      orientation: "h",
+    };
+
+    data = [trace];
+    let layout = {
+      title: " Top 10 OTU",
+      yaxis: {
+        tickmode: "linear",
+      },
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 30,
+      },
+    };
+
+    Plotly.newPlot("bar", data, layout);
+  });
 }
 //Initialize the display page
 function init() {
@@ -28,7 +72,9 @@ function init() {
     // display the data and the plots on the page
     // display demographic data
     getDemoInfo(data.names[0]);
+    id = data.names[0];
     //display plot
+    plotCharts(id);
   });
 }
 
