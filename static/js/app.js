0ae7d11420ee6f1bdc012765e35210143032258e
pparashar68 <pparashar68@gmail.com>
2021-01-04 20:13:42 -0800

    Added the code for pull subject id list and display demographic info

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,23 @@
 // Functions for events
-function getDemoInfo(names) {}
+function getDemoInfo(id) {
+  // Search jason file for input names value
+  console.log(id);
+  d3.json("samples.json").then((data) => {
+    metadata = data.metadata;
+    result = metadata.filter((meta) => meta.id.toString() === id)[0];
+    console.log(result);
+
+    let demographicInfo = d3.select("#sample-metadata");
+    demographicInfo.html("");
+
+    Object.entries(result).forEach((key) => {
+      demographicInfo.append("h5").text(key[0] + ": " + key[1] + "\n");
+    });
+  });
+}
+function optionChanged(id) {
+  getDemoInfo(id);
+}
 //Initialize the display page
 function init() {
   let dropdown = d3.select("#selDataset");
@@ -7,11 +25,11 @@ function init() {
     data.names.forEach(function (name) {
       dropdown.append("option").text(name).property("value");
     });
+    // display the data and the plots on the page
+    // display demographic data
+    getDemoInfo(data.names[0]);
+    //display plot
   });
-  // display the data and the plots on the page
-  // display demographic data
-  getDemoInfo(data.names[0]);
-  //display plot
 }
 
 init();
